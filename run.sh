acbeb0e3efecac32dfe17491bada1fcea7af8ebe
tony-o <tony.odell@xpertminds.net>
2015-12-03 16:49:14 -0800

    rename from .yaml

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 INCLUDE='-I../archive/perl6-module-does/lib -Itest/lib -Ilib'
 PLUGINS='-MA'
-CONFIG='test/test.yaml'
+CONFIG='test/test.config'
 # cannot test passing --plugins=XYZ because precomp is dumb.
 perl6 $INCLUDE $PLUGINS bin/scooter.pl6 $CONFIG
