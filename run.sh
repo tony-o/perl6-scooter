bdf0dcc9819fdda15b5b9bb05de81b13640f55a5
tony-o <tony.odell@xpertminds.net>
2015-12-03 16:47:57 -0800

    precomp notes

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 INCLUDE='-I../archive/perl6-module-does/lib -Itest/lib -Ilib'
 PLUGINS='-MA'
 CONFIG='test/test.yaml'
-echo perl6 $INCLUDE bin/scooter.pl6 --debug $PLUGINS $CONFIG
+# cannot test passing --plugins=XYZ because precomp is dumb.
 perl6 $INCLUDE $PLUGINS bin/scooter.pl6 $CONFIG
